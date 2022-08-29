f7460fff44ab4d502f23328d60d01334a49d19da
Tran-Van-Bong-95 <uchihaitachi888813@gmail.com>
2022-08-29 08:54:39 +0700

    lasted demo

--- src/Component/total/Total.js
+++ src/Component/total/Total.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './total.css'
 function Total() {
   const [scrollHeight, setScrollHeight] = useState(0)
